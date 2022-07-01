6e3d6577cd2421e3d13e24f5d1b66ab2a2729843
CHERYL ERH <im.shancezs@gmail.com>
2022-07-02 00:21:31 +0800

    Done

--- App.js
+++ App.js
@@ -16,13 +16,14 @@ function HomeScreen() {
 
   function addColor() {
     setColorArray([
-      ...colorArray,
+      
       {
         red: Math.floor(Math.random() * 256),
         green: Math.floor(Math.random() * 256),
         blue: Math.floor(Math.random() * 256),
         id: `${colorArray.length}`,
       },
+      ...colorArray,
     ]);
   }
 
